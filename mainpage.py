512a4af22064ff6bdd193c31db73f28c95c30af5
Miika Kalliokoski <kazmiika@cubbli18-gold.local>
2021-09-17 23:53:07 +0300

    Ravintolat ovat taas listattu suosion mukaan

--- mainpage.py
+++ mainpage.py
@@ -4,30 +4,26 @@ import sys
 from math import inf
 
 def render():
-    #restaurants = db.restaurants()
-    #restaurants.sort(key=sortByGrade)
-
-    dictionaries = zipper()
+    dictionaries = prepare()
     return render_template("index.html", dictionaries=dictionaries)
 
-
-def sort_by_grade(e):
-    #Descending
-    try:
-        return 0 - db.rating(e.id)
-    except:
-        return inf
-
-def zipper():
+def prepare():
     restaurants = db.restaurants()
     list = [None] * len(restaurants)
     i = 0
     for r in restaurants:
         list[i] = {"name": r.name, "rating": db.rating(r.id), "created_at": r.created_at, "id": r.id}
         i += 1
-    log(list)
-    #Sort list here
+    list.sort(key=sort_by_rating)
     return list
+
+def sort_by_rating(e):
+    #Descending
+    try:
+        #log(e["rating"])
+        return 0 - e["rating"]
+    except:
+        return inf
 
 def log(output):
     print("log:"+ str(output), file=sys.stdout)
