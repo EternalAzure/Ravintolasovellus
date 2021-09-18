52e6b0ff6423967b3fc72b8be46844dc50f569f8
Miika Kalliokoski <kazmiika@cubbli18-gold.local>
2021-09-18 04:05:11 +0300

    updated schema

--- schema.sql
+++ schema.sql
@@ -2,19 +2,21 @@ CREATE TABLE restaurants (
     id SERIAL PRIMARY KEY, 
     name TEXT, 
     created_at TIMESTAMP, 
-    address TEXT
+    address_id INTEGER REFERENCES addresses
 );
 
 CREATE TABLE review_categories (
     id SERIAL PRIMARY KEY,
-    category TEXT
+    category TEXT UNIQUE
 );
 
 CREATE TABLE grades (
     id SERIAL PRIMARY KEY,
     grade INTEGER,
-    restaurant_id INTEGER REFERENCES restaurants,
+    restaurant_id INTEGER REFERENCES restaurants
+    ON DELETE CASCADE,
     category_id INTEGER REFERENCES review_categories
+    ON DELETE CASCADE
 );
 
 CREATE TABLE reviews (
@@ -22,4 +24,25 @@ CREATE TABLE reviews (
     content TEXT,
     sent_at TIMESTAMP,
     restaurant_id INTEGER REFERENCES restaurants
+    ON DELETE CASCADE
+);
+
+CREATE TABLE streets (
+    id SERIAL PRIMARY KEY,
+    street TEXT UNIQUE
+);
+
+CREATE TABLE cities (
+    id SERIAL PRIMARY KEY,
+    city TEXT UNIQUE
+);
+
+CREATE TABLE addresses (
+    id SERIAL PRIMARY KEY,
+    street_id INTEGER 
+    REFERENCES streets
+    ON DELETE CASCADE,
+    city_id INTEGER 
+    REFERENCES cities
+    ON DELETE CASCADE
 );
