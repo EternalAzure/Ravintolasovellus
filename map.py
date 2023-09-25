1e684d2d29da67a05aceb2ede373edb309dca37c
Miika Kalliokoski <kall.miika@gmail.com>
2023-09-25 19:26:21 +0300

    Fix broken API call

--- map.py
+++ map.py
@@ -3,21 +3,18 @@ import requests
 
 key=getenv("MAPQUEST_KEY")
 
-def location(city, street):
-    url = "http://open.mapquestapi.com/nominatim/v1/search.php"
-    address = str(street + ", " + city)
-    load = {"key": key, "format": "json", "q": address, "countrycodes": "FI"}
-    response = requests.get(url, params=load)
-    data = {"lat": 0, "lon": 0} # Null Island
+def location(city, street) -> dict[str, float]:
+    url = f"https://www.mapquestapi.com/geocoding/v1/address?key={key}&json={{%22location%22:{{%22street%22:%22{street}%22,%22city%22:%22{city}%22}}}}"
+    response = requests.get(url)
+    data = {"lat": 0, "lng": 0} # Null Island
     try:
-        data = response.json()[0]
+        data = response.json()["results"][0]["locations"][0]["latLng"]
     except IndexError:
         pass
     except ValueError:
         print("Err")
         pass
 
-    coordinates = {"lat": float(data["lat"]), "lng": float(data["lon"])}
     
-    return coordinates
+    return data
 
